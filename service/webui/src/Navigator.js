971f5bb0cc616dd2ec6aaff07d1a7afdb0970d30
John Andersen <john.s.andersen@intel.com>
2019-11-04 16:31:59 -0800

    failing at removing scrollbar
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- service/webui/src/Navigator.js
+++ service/webui/src/Navigator.js
@@ -8,16 +8,10 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import HomeIcon from '@material-ui/icons/Home';
-import PeopleIcon from '@material-ui/icons/People';
 import DnsRoundedIcon from '@material-ui/icons/DnsRounded';
-import PermMediaOutlinedIcon from '@material-ui/icons/PhotoSizeSelectActual';
 import PublicIcon from '@material-ui/icons/Public';
 import SettingsEthernetIcon from '@material-ui/icons/SettingsEthernet';
-import SettingsInputComponentIcon from '@material-ui/icons/SettingsInputComponent';
-import TimerIcon from '@material-ui/icons/Timer';
 import SettingsIcon from '@material-ui/icons/Settings';
-import PhonelinkSetupIcon from '@material-ui/icons/PhonelinkSetup';
 import BlurOnIcon from '@material-ui/icons/BlurOn';
 import DesktopMacIcon from '@material-ui/icons/DesktopMac';
 import LayersIcon from '@material-ui/icons/Layers';
@@ -56,7 +50,8 @@ const categories = [
     id: 'Dataflows',
     children: [
       { id: 'View', icon: <BlurOnIcon /> },
-      { id: 'Create', icon: <SettingsEthernetIcon /> },
+      { id: 'Create', icon: <LayersIcon /> },
+      { id: 'Run', icon: <SettingsEthernetIcon /> },
       { id: 'Deploy', icon: <PublicIcon /> },
     ],
   },
@@ -113,8 +108,8 @@ function Navigator(props) {
   const { classes, ...other } = props;
 
   return (
-    <Drawer variant="permanent" {...other}>
-      <List disablePadding>
+    <Drawer variant="permanent" {...other} >
+      <List disablePadding >
         <ListItem className={clsx(classes.firebase, classes.item, classes.itemCategory)}>
           dataflow.ml
         </ListItem>
--- service/webui/src/theme.js
+++ service/webui/src/theme.js
@@ -4,16 +4,13 @@ import { createMuiTheme } from '@material-ui/core/styles';
 // A custom theme for this app
 const theme = createMuiTheme({
   '@global': {
+    "div": {
+      "-ms-overflow-style": "none",
+      "scrollbar-width": "none",
+    },
     '*::-webkit-scrollbar': {
-      width: '0'
+      display: "none",
     },
-    '*::-webkit-scrollbar-track': {
-      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
-    },
-    '*::-webkit-scrollbar-thumb': {
-      backgroundColor: 'rgba(0,0,0,.1)',
-      outline: '1px solid slategrey'
-    }
   },
   palette: {
     primary: {
