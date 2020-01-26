bd470bd8d392a627dc68da126f6f8d3f363db420
John Andersen <johnandersenpdx@gmail.com>
2020-01-26 14:11:37 -0800

    add fake server
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- service/webui/webui/src/index.js
+++ service/webui/webui/src/index.js
@@ -5,6 +5,9 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import Paperbase from './Paperbase';
 import theme from './theme';
 
+import server from './fakeserver';
+console.log("Using fake server", server);
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
